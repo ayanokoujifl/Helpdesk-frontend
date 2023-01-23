c19279950ea3abb4f5b7202705ff555bb85fb5bc
ayanokoujifl <guleite3@gmail.com>
2023-01-23 20:14:53 -0300

    Initialized tecnico-create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,7 +34,8 @@ import { HeaderComponent } from './components/header/header.component'
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component'
 import { LoginComponent } from './components/login/login.component'
 import { ToastrModule } from 'ngx-toastr'
-import { AuthInterceptorProvider } from './interceptors/auth.interceptor'
+import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component'
 
 @NgModule({
   declarations: [
@@ -43,7 +44,8 @@ import { AuthInterceptorProvider } from './interceptors/auth.interceptor'
     HomeComponent,
     HeaderComponent,
     TecnicoListComponent,
-    LoginComponent
+    LoginComponent,
+    TecnicoCreateComponent
   ],
   imports: [
     BrowserModule,
