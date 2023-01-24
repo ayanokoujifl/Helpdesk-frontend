d997d7a4026246deda9873196baf660cb72fa5b6
ayanokoujifl <guleite3@gmail.com>
2023-01-24 10:06:08 -0300

    Form validation TECNICO-CREATE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-
+import { NgxMaskModule } from 'ngx-mask'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
@@ -34,7 +34,7 @@ import { HeaderComponent } from './components/header/header.component'
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component'
 import { LoginComponent } from './components/login/login.component'
 import { ToastrModule } from 'ngx-toastr'
-import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
+import { AuthInterceptorProvider } from './interceptors/auth.interceptor'
 import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component'
 
 @NgModule({
@@ -75,7 +75,8 @@ import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecn
       timeOut: 4000,
       closeButton: true,
       progressBar: true
-    })
+    }),
+    NgxMaskModule.forRoot()
   ],
   providers: [AuthInterceptorProvider],
   bootstrap: [AppComponent]
